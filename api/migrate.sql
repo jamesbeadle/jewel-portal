8406714b2000d03f0c66f75941e546c12bc79562
Nigel Reilly <nigel.reilly@jewelgroup.co.uk>
2026-08-07 09:57:26 +0100

    Add migration

--- api/migrate.sql
+++ api/migrate.sql
@@ -1,1 +1,25 @@
-﻿
+﻿BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260807090000_AddRequestCriticalPathNudgeDismissed'
+)
+BEGIN
+    ALTER TABLE [Requests] ADD [CriticalPathNudgeDismissed] bit NOT NULL DEFAULT CAST(0 AS bit);
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260807090000_AddRequestCriticalPathNudgeDismissed'
+)
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20260807090000_AddRequestCriticalPathNudgeDismissed', N'8.0.10');
+END;
+GO
+
+COMMIT;
+GO
+
