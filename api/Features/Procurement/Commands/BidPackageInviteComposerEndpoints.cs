4fce14ca97486df355fe5f094013d5dbd09f2014
James Beadle <jamesbeadle1989@gmail.com>
2026-08-16 12:30:08 +0100

    Fix errors

--- api/Features/Procurement/Commands/BidPackageInviteComposerEndpoints.cs
+++ api/Features/Procurement/Commands/BidPackageInviteComposerEndpoints.cs
@@ -2,6 +2,7 @@ using Jewel.JPMS.Api.Cqrs;
 using Jewel.JPMS.Api.Gates;
 using Jewel.JPMS.Contracts.Cqrs;
 using Jewel.JPMS.Contracts.Procurement;
+using Jewel.JPMS.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
