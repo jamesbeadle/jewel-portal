70b4ff0df2558006c8ab377123330e58dabe5a8e
Nigel Reilly <nigel.reilly@jewelgroup.co.uk>
2026-08-27 11:33:43 +0100

    Migrate db

--- api/migrate.sql
+++ api/migrate.sql
@@ -3,38 +3,52 @@ GO
 
 IF NOT EXISTS (
     SELECT * FROM [__EFMigrationsHistory]
-    WHERE [MigrationId] = N'20260826180000_DropAgentTables'
+    WHERE [MigrationId] = N'20260827090000_AddCalendarEvents'
 )
 BEGIN
-    DROP TABLE [RequestAgents];
+    CREATE TABLE [CalendarEvents] (
+        [CalendarEventId] nvarchar(64) NOT NULL,
+        [ProjectId] nvarchar(64) NOT NULL,
+        [Number] int NOT NULL,
+        [Title] nvarchar(256) NOT NULL,
+        [Kind] int NOT NULL,
+        [Date] datetimeoffset NOT NULL,
+        [StartTime] nvarchar(5) NULL,
+        [EndDate] datetimeoffset NULL,
+        [Notes] nvarchar(max) NOT NULL,
+        [ClientVisible] bit NOT NULL,
+        [CreatedByEmail] nvarchar(256) NOT NULL,
+        [CreatedAt] datetimeoffset NOT NULL,
+        CONSTRAINT [PK_CalendarEvents] PRIMARY KEY ([CalendarEventId])
+    );
 END;
 GO
 
 IF NOT EXISTS (
     SELECT * FROM [__EFMigrationsHistory]
-    WHERE [MigrationId] = N'20260826180000_DropAgentTables'
+    WHERE [MigrationId] = N'20260827090000_AddCalendarEvents'
 )
 BEGIN
-    DROP TABLE [AgentChatMessages];
+    CREATE INDEX [IX_CalendarEvents_ProjectId_Date] ON [CalendarEvents] ([ProjectId], [Date]);
 END;
 GO
 
 IF NOT EXISTS (
     SELECT * FROM [__EFMigrationsHistory]
-    WHERE [MigrationId] = N'20260826180000_DropAgentTables'
+    WHERE [MigrationId] = N'20260827090000_AddCalendarEvents'
 )
 BEGIN
-    DROP TABLE [AgentProposals];
+    CREATE INDEX [IX_CalendarEvents_Number] ON [CalendarEvents] ([Number]);
 END;
 GO
 
 IF NOT EXISTS (
     SELECT * FROM [__EFMigrationsHistory]
-    WHERE [MigrationId] = N'20260826180000_DropAgentTables'
+    WHERE [MigrationId] = N'20260827090000_AddCalendarEvents'
 )
 BEGIN
     INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
-    VALUES (N'20260826180000_DropAgentTables', N'8.0.10');
+    VALUES (N'20260827090000_AddCalendarEvents', N'8.0.10');
 END;
 GO
 
