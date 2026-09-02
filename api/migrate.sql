4d5c360c793f581a638ce2174972ab5e3e52f507
Nigel Reilly <nigel.reilly@jewelgroup.co.uk>
2026-09-02 09:38:47 +0100

    Add migrations

--- api/migrate.sql
+++ api/migrate.sql
@@ -3,72 +3,41 @@ GO
 
 IF NOT EXISTS (
     SELECT * FROM [__EFMigrationsHistory]
-    WHERE [MigrationId] = N'20260827200000_DropAiChatTables'
+    WHERE [MigrationId] = N'20260902100000_WidenClaimPercentPrecision'
 )
 BEGIN
-    DROP TABLE [AiPendingReplies];
+    DECLARE @var0 sysname;
+    SELECT @var0 = [d].[name]
+    FROM [sys].[default_constraints] [d]
+    INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+    WHERE ([d].[parent_object_id] = OBJECT_ID(N'[ClaimLines]') AND [c].[name] = N'PercentComplete');
+    IF @var0 IS NOT NULL EXEC(N'ALTER TABLE [ClaimLines] DROP CONSTRAINT [' + @var0 + '];');
+    ALTER TABLE [ClaimLines] ALTER COLUMN [PercentComplete] decimal(28,20) NOT NULL;
 END;
 GO
 
 IF NOT EXISTS (
     SELECT * FROM [__EFMigrationsHistory]
-    WHERE [MigrationId] = N'20260827200000_DropAiChatTables'
+    WHERE [MigrationId] = N'20260902100000_WidenClaimPercentPrecision'
 )
 BEGIN
-    DROP TABLE [AiAttachments];
+    DECLARE @var1 sysname;
+    SELECT @var1 = [d].[name]
+    FROM [sys].[default_constraints] [d]
+    INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+    WHERE ([d].[parent_object_id] = OBJECT_ID(N'[ValuationReportSnapshotLines]') AND [c].[name] = N'PercentComplete');
+    IF @var1 IS NOT NULL EXEC(N'ALTER TABLE [ValuationReportSnapshotLines] DROP CONSTRAINT [' + @var1 + '];');
+    ALTER TABLE [ValuationReportSnapshotLines] ALTER COLUMN [PercentComplete] decimal(28,20) NOT NULL;
 END;
 GO
 
 IF NOT EXISTS (
     SELECT * FROM [__EFMigrationsHistory]
-    WHERE [MigrationId] = N'20260827200000_DropAiChatTables'
-)
-BEGIN
-    DROP TABLE [AiConversationMessages];
-END;
-GO
-
-IF NOT EXISTS (
-    SELECT * FROM [__EFMigrationsHistory]
-    WHERE [MigrationId] = N'20260827200000_DropAiChatTables'
-)
-BEGIN
-    DROP TABLE [AiConversations];
-END;
-GO
-
-IF NOT EXISTS (
-    SELECT * FROM [__EFMigrationsHistory]
-    WHERE [MigrationId] = N'20260827200000_DropAiChatTables'
+    WHERE [MigrationId] = N'20260902100000_WidenClaimPercentPrecision'
 )
 BEGIN
     INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
-    VALUES (N'20260827200000_DropAiChatTables', N'8.0.10');
-END;
-GO
-
-COMMIT;
-GO
-
-BEGIN TRANSACTION;
-GO
-
-IF NOT EXISTS (
-    SELECT * FROM [__EFMigrationsHistory]
-    WHERE [MigrationId] = N'20260828100000_AddOAuthClientSecret'
-)
-BEGIN
-    ALTER TABLE [OAuthClients] ADD [SecretHash] nvarchar(128) NULL;
-END;
-GO
-
-IF NOT EXISTS (
-    SELECT * FROM [__EFMigrationsHistory]
-    WHERE [MigrationId] = N'20260828100000_AddOAuthClientSecret'
-)
-BEGIN
-    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
-    VALUES (N'20260828100000_AddOAuthClientSecret', N'8.0.10');
+    VALUES (N'20260902100000_WidenClaimPercentPrecision', N'8.0.10');
 END;
 GO
 
